363d8c9afd6d60de069f230bb6f7ffe9a9b68dc9
J. Allen Burton <jburton6@my.westga.edu>
2020-01-18 09:34:43 -0500

    Added two more test methods to verify "Sunny Day" and othe boundry condition.

--- src/edu/westga/cs6312/mileage/testing/DigitWhenIncrementValue.java
+++ src/edu/westga/cs6312/mileage/testing/DigitWhenIncrementValue.java
@@ -19,5 +19,37 @@ class DigitWhenIncrementValue {
 		int value = theDigit.getValue();
 		assertEquals(1, value);
 	}
+	
+	/**
+	 * This method will confirm that incrementing Digit from 5
+	 * will produce a current value of 6 
+	 */
+	@Test
+	public void testIncrementDigit5ShouldGiveValue6() {
+		Digit theDigit = new Digit();
+		for (int count = 0; count < 5; count++) {
+			theDigit.increment();
+		}
+		
+		theDigit.increment();
+		int value = theDigit.getValue();
+		assertEquals(6, value);
+	}
+	
+	/**
+	 * This method will confirm that incrementing Digit from 9
+	 * will produce a current value of 0
+	 */
+	@Test
+	public void testIncrementDigit9ShouldGiveValue0() {
+		Digit theDigit = new Digit();
+		for (int count = 0; count < 9; count++) {
+			theDigit.increment();
+		}
+		
+		theDigit.increment();
+		int value = theDigit.getValue();
+		assertEquals(0, value);
+	}
 
 }
