90c01a52a057921cdd051fe9adaa48516a9d3804
J. Allen Burton <jburton6@my.westga.edu>
2020-01-20 08:46:03 -0500

    Changed implementation of Odometer toString method to use enhanced for loop and substring() and added decrement method.  decrement method still needs tests built.

--- src/edu/westga/cs6312/mileage/model/Odometer.java
+++ src/edu/westga/cs6312/mileage/model/Odometer.java
@@ -11,48 +11,52 @@ import java.text.DecimalFormat;
  */
 public class Odometer {
 	private Digit[] digitArray;
-	
+
 	/**
-	 * Constructor for Odometer objects.  Creates a 4 digit odometer set at 000.0
+	 * Constructor for Odometer objects. Creates a 4 digit odometer set at 000.0
 	 * 
-	 * @precondition	none
+	 * @precondition none
 	 */
 	public Odometer() {
 		this(0, 0, 0, 0);
 	}
-	
+
 	/**
-	 *  Constructor for Odometer objects.  Creates a 4 digit odometer set using 
-	 * the digits supplied in the paramaters.
+	 * Constructor for Odometer objects. Creates a 4 digit odometer set using the
+	 * digits supplied in the paramaters.
 	 * 
-	 * @param hundredsPlaceDigit	int to represent the hundreds place in the initial odometer reading
-	 * @param tensPlaceDigit		int to represent the tens place in the initial odometer reading
-	 * @param onesPlaceDigit		int to represent the ones place in the initial odometer reading
-	 * @param decimalDigit			int to represent the tenths (decimal) place in the initial odometer reading
+	 * @param hundredsPlaceDigit int to represent the hundreds place in the initial
+	 *                           odometer reading
+	 * @param tensPlaceDigit     int to represent the tens place in the initial
+	 *                           odometer reading
+	 * @param onesPlaceDigit     int to represent the ones place in the initial
+	 *                           odometer reading
+	 * @param decimalDigit       int to represent the tenths (decimal) place in the
+	 *                           initial odometer reading
 	 * 
-	 * @precondition	hundredsPlaceDigit >= 0 && hundredsPlaceDigit < 10
-	 * @precondition	tensPlaceDigit >= 0 && hundredsPlaceDigit < 10
-	 * @precondition	onesPlaceDigit >= 0 && hundredsPlaceDigit < 10
-	 * @precondition	decimalDigit >= 0 && hundredsPlaceDigit < 10
+	 * @precondition hundredsPlaceDigit >= 0 && hundredsPlaceDigit < 10
+	 * @precondition tensPlaceDigit >= 0 && hundredsPlaceDigit < 10
+	 * @precondition onesPlaceDigit >= 0 && hundredsPlaceDigit < 10
+	 * @precondition decimalDigit >= 0 && hundredsPlaceDigit < 10
 	 */
 	public Odometer(int hundredsPlaceDigit, int tensPlaceDigit, int onesPlaceDigit, int decimalDigit) {
 		this.digitArray = new Digit[4];
-		this.setDigitValue(0, hundredsPlaceDigit);		
-		this.setDigitValue(1, tensPlaceDigit);	
-		this.setDigitValue(2, onesPlaceDigit);	
-		this.setDigitValue(3, decimalDigit);	
+		this.setDigitValue(0, hundredsPlaceDigit);
+		this.setDigitValue(1, tensPlaceDigit);
+		this.setDigitValue(2, onesPlaceDigit);
+		this.setDigitValue(3, decimalDigit);
 	}
-	
+
 	/**
-	 * This helper method sets a specified digit to a specified value on the odometer.  Digits
-	 * are ordered from left to right begining with digit 0 (i.e. digit 0 is the hundreth place
-	 * for a 4 digit odometer: xxx.x) 
+	 * This helper method sets a specified digit to a specified value on the
+	 * odometer. Digits are ordered from left to right begining with digit 0 (i.e.
+	 * digit 0 is the hundreth place for a 4 digit odometer: xxx.x)
 	 * 
 	 * @param digitPosition
 	 * @param digitValue
 	 * 
-	 * @precondition	digitPosition < 4
-	 * @precondition	digitValue >= 0 && digitValue < 10
+	 * @precondition digitPosition < 4
+	 * @precondition digitValue >= 0 && digitValue < 10
 	 */
 	private void setDigitValue(int digitPosition, int digitValue) {
 		this.digitArray[digitPosition] = new Digit();
@@ -60,32 +64,57 @@ public class Odometer {
 			this.digitArray[digitPosition].increment();
 		}
 	}
-	
+
 	/**
-	 * Method to return a string containing a message stating the current odometer reading
+	 * Method to return a string containing a message stating the current odometer
+	 * reading
 	 * 
 	 * @return String containing a statement including the current odometer reading
 	 * 
-	 * @precondition	none
+	 * @precondition none
 	 */
 	public String toString() {
 		String odometerDigitsString = "";
-		for (int currentPosition = 0; currentPosition < this.digitArray.length; currentPosition++) {
-			if (currentPosition == (this.digitArray.length - 1)) {
-				odometerDigitsString += ".";
-			}
-			odometerDigitsString += this.digitArray[currentPosition].getValue();
-		}
-		return "Odometer with mileage " + odometerDigitsString;
-		
-		
-		/*
 		for (Digit value : this.digitArray) {
 			odometerDigitsString += value.getValue();
 		}
-		Double odometerReading = Double.parseDouble(odometerDigitsString);
-		DecimalFormat decim = new DecimalFormat("000.0");
-		return "Odometer with mileage " + decim.format(odometerReading);
-		*/
+		odometerDigitsString = odometerDigitsString.substring(0, (odometerDigitsString.length() - 1)) + "."
+				+ odometerDigitsString.substring((odometerDigitsString.length() - 1), odometerDigitsString.length());
+		return "Odometer with mileage " + odometerDigitsString;
 	}
+
+	/**
+	 * This method will cause the specified dial on the odometer to advance the
+	 * number of turns specified. The position references dials from left with 0
+	 * being the left-most dial.
+	 * 
+	 * @param position
+	 * @param numberOfTurns
+	 * 
+	 * @precondition position < 4
+	 * @precondition numberOfTurns >= 0
+	 */
+	public void incrementDigit(int position, int numberOfTurns) {
+		for (int incrementLevel = 0; incrementLevel < numberOfTurns; incrementLevel++) {
+			this.digitArray[position].increment();
+		}
+	}
+	
+	/**
+	 * This method will cause the specified dial on the odometer to roll back the
+	 * number of turns specified. The position references dials from left with 0
+	 * being the left-most dial.
+	 * 
+	 * @param position
+	 * @param numberOfTurns
+	 * 
+	 * @precondition position < 4
+	 * @precondition numberOfTurns >= 0
+	 */
+	public void decrementDigit(int position, int numberOfTurns) {
+		for (int incrementLevel = 0; incrementLevel < numberOfTurns; incrementLevel++) {
+			this.digitArray[position].decrement();
+		}
+	}
+	
 }
