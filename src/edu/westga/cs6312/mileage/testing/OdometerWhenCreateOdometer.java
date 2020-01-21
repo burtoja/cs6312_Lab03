c2a1a1ded65d581233eb1ada30140affca502e8a
J. Allen Burton <jburton6@my.westga.edu>
2020-01-21 12:40:53 -0500

    Added a few more tests to Constructor and Increment testClasses and organized for submittal.

--- src/edu/westga/cs6312/mileage/testing/OdometerWhenCreateOdometer.java
+++ src/edu/westga/cs6312/mileage/testing/OdometerWhenCreateOdometer.java
@@ -32,18 +32,6 @@ public class OdometerWhenCreateOdometer {
 	 * as parameters
 	 */
 	@Test
-	public void test4ParameterConstructorShouldHaveMileage4893() {
-		Odometer theOdometer = new Odometer(4, 8, 9, 3);
-		String value = theOdometer.toString();
-		assertEquals("Odometer with mileage 489.3", value);
-	}
-	
-	/**
-	 * Test to be sure that the Odometer's 4 parameter constructor will
-	 * correctly create an Odometer showing 489.3 when given the 4, 8, 9, 3 
-	 * as parameters
-	 */
-	@Test
 	public void test4ParameterConstructorMinimumShouldHaveMileage0000() {
 		Odometer theOdometer = new Odometer(0, 0, 0, 0);
 		String value = theOdometer.toString();
@@ -63,13 +51,37 @@ public class OdometerWhenCreateOdometer {
 	}
 	
 	/**
+	 * Test to be sure that the Odometer's 4 parameter constructor will
+	 * correctly create an Odometer showing 489.3 when given the 4, 8, 9, 3 
+	 * as parameters
+	 */
+	@Test
+	public void test4ParameterConstructorShouldHaveMileage4893() {
+		Odometer theOdometer = new Odometer(4, 8, 9, 3);
+		String value = theOdometer.toString();
+		assertEquals("Odometer with mileage 489.3", value);
+	}
+	
+	/**
 	 * Test to be sure that the Odometer's 4 parameter constructor precondition
 	 * check works.  Delivering negative number as parameter as first parameter 
-	 * should default that digit to zero (099.0)
+	 * should default the entire odometer to zero (000.0)
 	 */
 	@Test
 	public void test4ParameterConstructorWithNeg1ShouldHaveMileage0000() {
 		Odometer theOdometer = new Odometer(-1, 9, 9, 9);
+		String value = theOdometer.toString();
+		assertEquals("Odometer with mileage 000.0", value);
+	}
+	
+	/**
+	 * Test to be sure that the Odometer's 4 parameter constructor precondition
+	 * check works.  Delivering a number > 9 as parameter as first parameter 
+	 * should default the entire odometer to zero (000.0)
+	 */
+	@Test
+	public void test4ParameterConstructorWith12In100sPlaceShouldHaveMileage0000() {
+		Odometer theOdometer = new Odometer(12, 9, 9, 9);
 		String value = theOdometer.toString();
 		assertEquals("Odometer with mileage 000.0", value);
 	}
--- src/edu/westga/cs6312/mileage/testing/OdometerWhenIncrementDigit.java
+++ src/edu/westga/cs6312/mileage/testing/OdometerWhenIncrementDigit.java
@@ -16,16 +16,39 @@ import edu.westga.cs6312.mileage.model.Odometer;
 public class OdometerWhenIncrementDigit {
 
 	/**
-	 * Test to be sure IncrementDigit advances the 10ths place on a
+	 * Test to be sure incrementDigit advances the 10ths place (decimal) on a
 	 * new Odometer (000.0) by 5 turns showing 000.5
-	 *
 	 */
 	@Test
-	public void test10sPlaceIncrementBy5ShouldHave0005() {
+	public void test10thsPlaceIncrementBy5ShouldHave0005() {
 		Odometer theOdometer = new Odometer();
 		theOdometer.incrementDigit(3, 5);
 		String value = theOdometer.toString();
 		assertEquals("Odometer with mileage 000.5", value);
+	}
+	
+	/**
+	 * Test to be sure incrementDigit advances the ones place on a
+	 * new Odometer (000.0) by 5 turns showing 005.0
+	 */
+	@Test
+	public void test1sPlaceIncrementBy5ShouldHave0050() {
+		Odometer theOdometer = new Odometer();
+		theOdometer.incrementDigit(2, 5);
+		String value = theOdometer.toString();
+		assertEquals("Odometer with mileage 005.0", value);
+	}
+	
+	/**
+	 * Test to be sure incrementDigit advances the 100s place on a
+	 * new Odometer (000.0) by 5 turns showing 500.0
+	 */
+	@Test
+	public void test100sPlaceIncrementBy5ShouldHave5000() {
+		Odometer theOdometer = new Odometer();
+		theOdometer.incrementDigit(0, 5);
+		String value = theOdometer.toString();
+		assertEquals("Odometer with mileage 500.0", value);
 	}
 	
 	/**
@@ -43,9 +66,32 @@ public class OdometerWhenIncrementDigit {
 	}
 	
 	/**
+	 * Test to be sure incrementDigit advances the 100s place on a
+	 * new Odometer (000.0) by 12 turns showing 200.0
+	 */
+	@Test
+	public void test100sPlaceIncrementBy12ShouldHave2000() {
+		Odometer theOdometer = new Odometer();
+		theOdometer.incrementDigit(0, 12);
+		String value = theOdometer.toString();
+		assertEquals("Odometer with mileage 200.0", value);
+	}
+	
+	/**
+	 * Test to be sure incrementDigit advances the 10ths (decimal) place on a
+	 * new Odometer (000.0) by 12 turns showing 200.0
+	 */
+	@Test
+	public void test10thsPlaceIncrementBy12ShouldHave0012() {
+		Odometer theOdometer = new Odometer();
+		theOdometer.incrementDigit(3, 12);
+		String value = theOdometer.toString();
+		assertEquals("Odometer with mileage 001.2", value);
+	}
+	
+	/**
 	 * Test to be sure that when IncrementDigit advances the 10ths (decimal) place on a
-	 * Odometer initialized at 999.9 by 5 turns it will show 000.4
-	 *
+	 * Odometer initialized at 999.9 by 5 turns it will roll over and show 000.4
 	 */
 	@Test
 	public void testInitial9999Increment10thsPlaceBy5ShouldHave0004() {
@@ -57,8 +103,8 @@ public class OdometerWhenIncrementDigit {
 	
 	/**
 	 * Test to be sure that when IncrementDigit advances the ones place on a
-	 * Odometer initialized at 999.9 by 5 turns it will show 000.4
-	 *
+	 * Odometer initialized at 999.9 by 5 turns it will rollover the dials to the 
+	 * left and show 004.9
 	 */
 	@Test
 	public void testInitial9999Increment1sPlaceBy5ShouldHave0049() {
@@ -69,22 +115,47 @@ public class OdometerWhenIncrementDigit {
 	}
 	
 	/**
-	 * Test to be sure IncrementDigit advances the 10ths (decimal) place on a
-	 * new Odometer (000.0) by 12 turns showing 001.2
-	 *
+	 * Test to be sure that when IncrementDigit advances the 100s place on a
+	 * Odometer initialized at 999.9 by 5 turns it will rollover  only that 
+	 * dial to the left and show 499.9
 	 */
 	@Test
-	public void test10thsPlaceIncrementBy12ShouldHave0002() {
-		Odometer theOdometer = new Odometer();
-		theOdometer.incrementDigit(3, 12);
+	public void testInitial9999Increment100sPlaceBy5ShouldHave4999() {
+		Odometer theOdometer = new Odometer(9, 9, 9, 9);
+		theOdometer.incrementDigit(0, 5);
 		String value = theOdometer.toString();
-		assertEquals("Odometer with mileage 001.2", value);
+		assertEquals("Odometer with mileage 499.9", value);
 	}
 	
 	/**
+	 * Test to be sure that when IncrementDigit advances properly for a large 
+	 * number of turns.  Odometer initialized at 000.0 incremented by 5678 turns 
+	 * will show 499.9
+	 */
+	@Test
+	public void testInitial0000Increment10thsPlaceBy5678ShouldHave5678() {
+		Odometer theOdometer = new Odometer();
+		theOdometer.incrementDigit(3, 5678);
+		String value = theOdometer.toString();
+		assertEquals("Odometer with mileage 567.8", value);
+	}
+	
+	/**
+	 * Test to be sure that when IncrementDigit advances properly for a large 
+	 * number of turns with rollo ver.  Odometer initialized at 000.0 incremented by 25678 turns 
+	 * will show 499.9
+	 */
+	@Test
+	public void testInitial0000Increment10thsPlaceBy25678ShouldHave5678() {
+		Odometer theOdometer = new Odometer();
+		theOdometer.incrementDigit(3, 25678);
+		String value = theOdometer.toString();
+		assertEquals("Odometer with mileage 567.8", value);
+	}
+		
+	/**
 	 * Test of precondition.  Odometer set at 555.5. Given dial -1.  
 	 * Odometer should stay at 555.5.
-	 *
 	 */
 	@Test
 	public void testPreconditionOdometerSetAt5555ChooseDialNeg1ShouldHave5555() {
